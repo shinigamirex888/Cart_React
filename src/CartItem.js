71ce17ac770ffd52e198c533c5b89d909a01f9a1
Snehil <snehilsneh888@gmail.com>
2021-04-26 13:57:07 +0530

    Created CartItem class and added jsx

---
+++ src/CartItem.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class CartItem extends React.Component{
+    render(){
+        return(
+          <div className="cart-item">
+              <div className="left-block">
+                <img style={styles.image}></img>
+              </div>
+              <div className="right-block">
+                  <div style={{fontSize:25}}>Phone</div>
+                  <div style={{color:'grey'}}>999</div>
+                  <div style={{color:'grey'}}>Qty: 1</div>
+                  <div className="cart-item-actions">
+                      {/* {Buttons} */}
+                  </div>
+
+              </div>
+          </div>  
+            
+        );
+    }
+}
+
+const styles={
+    image:{
+        height:110,
+        width:110,
+        borderRadius:4,
+        backgroundColor:'#ccc'
+    }
+}
+
+export default CartItem;
